3d3f3944e28828903faa0b7269efa20af3a121c8
czheng17 <czheng17@binghamton.edu>
2017-04-13 19:31:57 -0400

    Construct Binary Tree
    Construct Binary Tree from Inorder and Postorder Traversal

--- src/com/aslan/middle/L_106.java
+++ src/com/aslan/middle/L_106.java
@@ -1,5 +1,31 @@
 package com.aslan.middle;
 
 public class L_106 {
-
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return this.buildMyTree(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
+    }
+	
+	private TreeNode buildMyTree(int[] inorder,int instart,int inend,int[] postorder,int poststart,int postend){
+		if(instart>inend || poststart>postend){
+			return null;
+		}
+		
+		int rootval = postorder[postend];
+		TreeNode root =new TreeNode(rootval);
+		
+		int inorder_root_index=0;
+		for(int i=instart;i<=inend;i++){
+			if(inorder[i]==rootval){
+				inorder_root_index = i;
+				break;
+			}
+		}
+		int len = inorder_root_index - instart;
+		
+		root.left = this.buildMyTree(inorder, instart, inorder_root_index-1, postorder, poststart, poststart+len-1);
+		
+		root.right = this.buildMyTree(inorder, inorder_root_index+1, inend, postorder, poststart+len, postend-1);
+		
+		return root;
+	}
 }

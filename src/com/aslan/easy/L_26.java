41d7074ef8ba86da400d5dcf576f6cfd5a124cf8
czheng17 <czheng17@binghamton.edu>
2017-04-17 21:17:29 -0400

    Remove Duplicates from Sorted Array
    Remove Duplicates from Sorted Array

--- src/com/aslan/easy/L_26.java
+++ src/com/aslan/easy/L_26.java
@@ -1,9 +1,25 @@
 package com.aslan.easy;
+/*26. Remove Duplicates from Sorted Array
+ * Given a sorted array, remove the duplicates in place 
+ * such that each element appear only once and return the new length.
 
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums 
+being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+ * 
+ * */
 import java.util.Arrays;
 
 public class L_26 {
-    public int removeDuplicates(int[] nums) {
-        Arrays.sort(nums);
+    public int removeDuplicates(int[] A) {
+        if (A.length==0) return 0;
+        int j=0;
+        for (int i=0; i<A.length; i++)
+            if (A[i]!=A[j]) A[++j]=A[i];
+        return ++j;
     }
 }

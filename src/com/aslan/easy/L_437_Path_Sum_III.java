0e3ace4311fbee0783cf04704de4caafa2ea94da
czheng17 <czheng17@binghamton.edu>
2017-09-14 20:56:57 -0400

    Create L_437_Path_Sum_III.java

---
+++ src/com/aslan/easy/L_437_Path_Sum_III.java
@@ -0,0 +1,46 @@
+package com.aslan.easy;
+/*
+这道题就是给了一个二叉树和一个目标和sum 
+找出所有路径，这个路径的和等于sum，只允许从父节点到子节点的路线
+You are given a binary tree in which each node contains an integer value.
+Find the number of paths that sum to a given value.
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000. 
+Example: 
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+public class L_437_Path_Sum_III {
+	public int pathSum(TreeNode root, int sum) {
+        if(root == null){
+        	return 0;
+        }
+        return this.dfs_method(root,sum) + this.pathSum(root.left, sum) + this.pathSum(root.right, sum);
+    }
+	private int dfs_method(TreeNode root, int sum) {
+		int res = 0;
+		if(root == null){
+			return res;
+		}
+		if(root.val == sum){
+			res++;
+		}
+		res += this.dfs_method(root.left, sum-root.val);
+		res += this.dfs_method(root.right, sum-root.val);
+		
+		return res;
+	}
+}
